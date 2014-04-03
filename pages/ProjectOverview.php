8080cb8eba66c9b8f4d2cffc97edcc18fd4add27
HannTu <h.tulowiecka@gmail.com>
2014-04-03 22:50:58 +0200

    Update ProjectOverview.php

--- pages/ProjectOverview.php
+++ pages/ProjectOverview.php
@@ -6,11 +6,63 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
         <div class="container">
-                <div class="table-background-white col-md-6" style="height:420px;">
-                Tu jakies overview projektu.... W redminie jest to Issue Tracking...
-                </div>
+                <div class="table-background-white col-md-6" style="height:520px;">
+
+           <div class="tab-content">
+  <span>                  
+<h3>Nazwa projektu</h3>
+<h4>#ID projektu</h4> </span>
+                                <h4><span class="glyphicon glyphicon-info-sign"></span> Szczegóły</h4>
+                                <div class="row">
+                                       
+                                        <div class="col-md-2 col-md-3 details-label">Status:</div>
+                                        <div class="col-md-8 col-md-6">W trakcie realizacji</div>
+                                </div>
+                                <div class="row show-grid row-with-space">
+                                      
+                                        <div class="col-md-2 col-md-3 details-label">Kategoria:</div>
+                                        <div class="col-md-5 col-md-6">Strona WWW</div>
+                                </div>
+                                <div class="row show-grid row-with-space">
+                                        <div class="col-md-2 col-md-3 details-label">Wersja:</div>
+                                        <div class="col-md-2 col-md-6">3.0</div>
+                                      
+                                </div>
+                                <h4><span class="glyphicon glyphicon-font"></span> Opis</h4>
+                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam semper eros id massa pretium varius. Phasellus iaculis placerat rutrum. Maecenas facilisis odio libero, non tincidunt turpis ultricies id. Nam scelerisque ligula vel est posuere, ultricies imperdiet elit eleifend. Etiam eu nibh sem. Vestibulum laoreet sollicitudin lorem, nec facilisis orci posuere sit amet. Aenean suscipit risus elementum eros vehicula consectetur. Sed eu venenatis nisl, et accumsan lorem. Etiam ac mollis sapien. Fusce scelerisque orci non felis volutpat porta. Suspendisse at nisi.
+                                                            
+                                </div>
+                                                </div>
                 <div class="table-background-white col-md-5 col-md-offset-1" style="height:420px;">
-                I Membersi...
+                 <table class="table table-bordered">
+                        <thead>
+                               
+                               <tr>
+										<th>Rola</th>	                                        
+										<th>Imię i nazwisko</th>
+                                        <th>Username</th>
+                                        <th>Status</th>
+                                       
+                                 </tr>
+                               
+                        </thead>
+                        <tbody>
+                                <tr  class="table-clickable">
+                                        <td>Kierownik projektu</td>
+                                        <td>Adam Nowak</td>
+                                        <td>@adnow</td>
+                                        <td>Aktywny</td>
+                               
+                                      </tr> 
+     							 <tr  class="table-clickable">
+                                        <td>Developer</td>
+                                        <td>Piotr Kowalski</td>
+                                        <td>@alan</td>
+                                        <td>Aktywny</td>
+                               
+                                      </tr>
+
+</table>
                 </div>
         </div>
 </div>
