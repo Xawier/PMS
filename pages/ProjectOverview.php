b228eee1ec93e51f00f76f55b03cb378b2a63157
HannTu <h.tulowiecka@gmail.com>
2014-04-06 23:24:50 +0200

    Update ProjectOverview.php

--- pages/ProjectOverview.php
+++ pages/ProjectOverview.php
@@ -6,63 +6,151 @@
 <!-- Main jumbotron for a primary marketing message or call to action -->
 <div class="jumbotron">
         <div class="container">
-                <div class="table-background-white col-md-6" style="height:520px;">
-
-           <div class="tab-content">
+                <div class="tab-content col-md-6 " style="height:520px;">
   <span>                  
-<h3>Nazwa projektu</h3>
-<h4>ID projektu</h4> </span>
-                                <h4><span class="glyphicon glyphicon-info-sign"></span> Szczegóły</h4>
-                                <div class="row">
-                                       
-                                        <div class="col-md-2 col-md-3 details-label">Status:</div>
-                                        <div class="col-md-8 col-md-6">W trakcie realizacji</div>
-                                </div>
-                                <div class="row show-grid row-with-space">
-                                      
-                                        <div class="col-md-2 col-md-3 details-label">Kategoria:</div>
-                                        <div class="col-md-5 col-md-6">Strona WWW</div>
-                                </div>
-                                <div class="row show-grid row-with-space">
-                                        <div class="col-md-2 col-md-3 details-label">Wersja:</div>
-                                        <div class="col-md-2 col-md-6">3.0</div>
-                                      
-                                </div>
-                                <h4><span class="glyphicon glyphicon-font"></span> Opis</h4>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam semper eros id massa pretium varius. Phasellus iaculis placerat rutrum. Maecenas facilisis odio libero, non tincidunt turpis ultricies id. Nam scelerisque ligula vel est posuere, ultricies imperdiet elit eleifend. Etiam eu nibh sem. Vestibulum laoreet sollicitudin lorem, nec facilisis orci posuere sit amet. Aenean suscipit risus elementum eros vehicula consectetur. Sed eu venenatis nisl, et accumsan lorem. Etiam ac mollis sapien. Fusce scelerisque orci non felis volutpat porta. Suspendisse at nisi.
-                                                            
-                                </div>
-                                                </div>
-                <div class="table-background-white col-md-5 col-md-offset-1" style="height:420px;">
-                 <table class="table table-bordered">
+											<h3>Nazwa projektu</h3>
+											<h4>#ID projektu</h4> </span>
+					<!--Nav tabs-->
+					<ul class="nav nav-tabs">
+					  <li class="active"><a href="#projekt" data-toggle="tab">Projekt</a></li>
+					  <li><a href="#user" data-toggle="tab">Użytkownicy</a></li>
+					  <li><a href="#taski" data-toggle="tab">Taski</a></li>
+					</ul>
+						<!--Content tabs-->
+						<div class="tab-content tab-content-white">					   
+						<div id="projekt" class="tab-pane active">
+											
+						                    <h4><span class="glyphicon glyphicon-info-sign"></span> Szczegóły</h4>
+						                    <div class="row">
+						                           
+						                            <div class="col-md-2 col-md-3 details-label">Status:</div>
+						                            <div class="col-md-8 col-md-6">W trakcie realizacji</div>
+						                    </div>
+						                    <div class="row show-grid row-with-space">
+						                          
+						                            <div class="col-md-2 col-md-3 details-label">Kategoria:</div>
+						                            <div class="col-md-5 col-md-6">Strona WWW</div>
+						                    </div>
+						                    <div class="row show-grid row-with-space">
+						                            <div class="col-md-2 col-md-3 details-label">Wersja:</div>
+						                            <div class="col-md-2 col-md-6">3.0</div>
+						                          
+						                    </div>
+						                    <h4><span class="glyphicon glyphicon-font"></span> Opis</h4>
+						                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam semper eros id massa pretium varius. Phasellus iaculis placerat rutrum. Maecenas facilisis odio libero, non tincidunt turpis ultricies id. Nam scelerisque ligula vel est posuere, ultricies imperdiet elit eleifend. Etiam eu nibh sem. Vestibulum laoreet sollicitudin lorem, nec facilisis orci posuere sit amet. Aenean suscipit risus elementum eros vehicula consectetur. Sed eu venenatis nisl, et accumsan lorem. Etiam ac mollis sapien. Fusce scelerisque orci non felis volutpat porta. Suspendisse at nisi.
+						                                                
+						 </div> 	
+						<div id="user" class="tab-pane">					         
+						     <table class="table table-bordered">
+						            <thead>
+						                   
+						                   <tr>
+													<th>Rola</th>	                                        
+													<th>Imię i nazwisko</th>
+						                            <th>Username</th>
+						                            <th>Status</th>
+						                           
+						                     </tr>
+						                   
+						            </thead>
+						            <tbody>
+						                    <tr  class="table-clickable">
+						                            <td>Kierownik projektu</td>
+						                            <td>Adam Nowak</td>
+						                            <td>@adnow</td>
+						                            <td>Aktywny</td>
+						                   
+						                          </tr> 
+				 							 <tr  class="table-clickable">
+						                            <td>Developer</td>
+						                            <td>Piotr Kowalski</td>
+						                            <td>@alan</td>
+						                            <td>Aktywny</td>
+						                   
+						                          </tr>
+
+			</table></div>
+
+			<div id="taski" class="tab-pane">
+											<table class="table table-hover">
                         <thead>
-                               
-                               <tr>
-										<th>Rola</th>	                                        
-										<th>Imię i nazwisko</th>
-                                        <th>Username</th>
-                                        <th>Status</th>
-                                       
-                                 </tr>
-                               
+                                <tr>
+                                        <th>Typ</th>
+                                        <th>Klucz</th>
+                                        <th>Podsumowanie</th>
+                                        <th>Os. przypisana</th>
+										<th>Deadline</th>
+                                </tr>
                         </thead>
                         <tbody>
                                 <tr  class="table-clickable">
-                                        <td>Kierownik projektu</td>
-                                        <td>Adam Nowak</td>
-                                        <td>@adnow</td>
-                                        <td>Aktywny</td>
-                               
-                                      </tr> 
-     							 <tr  class="table-clickable">
-                                        <td>Developer</td>
-                                        <td>Piotr Kowalski</td>
-                                        <td>@alan</td>
-                                        <td>Aktywny</td>
-                               
-                                      </tr>
+                                        <td>Bug</td>
+                                        <td>#BUG-1</td>
+                                        <td>Zadanie pierwsze, coś nie działa</td>
+                                        <td>Michał Kot</td>
+                                        <td>31.03.2014</td>
+                                </tr>
+                                <tr class="table-clickable">
+                                        <td>Zadanie</td>
+                                        <td>#TASK-1</td>
+                                        <td>Funkcjonalnośc do implementacji</td>
+                                        <td>Tadek Ruczyński</td>
+									    <td>04.04.2014</td>
+                                </tr>
+                                <tr class="table-clickable">
+                                        <td>Klient</td>
+                                        <td>#BUG-K1</td>
+                                        <td>Problem z czymśtam</td>
+                                        <td>Wacław Puka</td>
+                                		<td> --- </td>
+                                </tr>
+                                <tr class="table-clickable">
+                                        <td>Bug</td>
+                                        <td>#BUG-2</td>
+                                        <td>Zaś coś nie działa</td>
+                                        <td>Michał Flaming</td>
+                                  		<td>30.01.2014</td>
+										
+                                </tr>
+                        </tbody>
+                </table>
+					                                                
+		  </div> </div></div>
+						        
+       <div class="container">
+              <div class="table-background-white col-md-5 col-md-offset-1" style="height:420px;">Ostatnie zmiany w projekcie 
+					 <table class="table table-bordered">
+						            <thead>
+						                   
+						                   <tr>
+													<th>Zmiana</th>	 
+													  <th>Opis</th>                                       
+													<th>User</th>
+						                            <th>Data</th>
+						                         
+						                           
+						                     </tr>
+						                   
+						            </thead>
+						            <tbody>
+						                    <tr  class="table-clickable">
+						                            <td>Task #BUG</td>
+						                            <td>dodanie nowej funkcjonalności</td>
+						                            <td>@adnow</td>
+						                            <td>5.04.2014</td>
+						                   
+						                          </tr> 
+				 							 <tr  class="table-clickable">
+						                            <td>Dodanie usera</td>
+						                            <td>dodanie nowej osoby do projektu</td>
+						                            <td>@alan</td>
+						                            <td>30.03.2014</td>
+						                   
+						                          </tr>
 
-</table>
+											</table>
+								</div>
+      
                 </div>
-        </div>
-</div>
+               
+                </div>  </div>
