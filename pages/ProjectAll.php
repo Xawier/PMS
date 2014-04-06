0a7a03bed8adfe8c1c4bfee57dbaa045d47a3bbd
HannTu <h.tulowiecka@gmail.com>
2014-04-06 22:58:20 +0200

    Create ProjectAll.php

---
+++ pages/ProjectAll.php
@@ -0,0 +1,64 @@
+<div class="container">
+        <div class="half-space-container">
+                <ol class="breadcrumb">
+                        <li class="active">Zadania</li>
+                </ol>
+        </div>
+       
+</div>
+<!-- Main jumbotron for a primary marketing message or call to action -->
+<div class="jumbotron">
+        <div class="container">
+                <div class="center-block table-responsive table-background-white">
+                <table class="table table-hover">
+                        <thead>
+                                <tr>
+                                        <th>Nazwa</th>
+                                        <th>ID</th>
+                                        <th>Opis</th>
+                                        <th>Homepage</th>
+                                        <th>Jest publiczny</th>
+                                        <th>Status</th>
+                                        <th>Utworzono</th>
+                                        <th>Uaktualniono</th>
+                                        
+                                </tr>
+                        </thead>
+                        <tbody>
+                                <tr  class="table-clickable-project">
+                                        <td>Sklep internetowy</td>
+                                        <td>123</td>
+                                        <td>Stworzenie sklepu internetowego</td>
+                                        <td> ..</td>
+                                        <td>TAK</td>
+                                        <td>W trakcie realizacji</td>
+                                        <td>13.03.2014</td>
+                                        <td>31.03.2014</td>
+                                </tr>
+                                <tr class="table-clickable-project">
+                                        <td>Aplikacja komórkowa</td>
+                                        <td>112</td>
+                                        <td>Aplikacja do zarządzania wiadomościami</td>
+                                        <td>..</td>
+                                        <td>NIE</td>
+                                        <td>Zakończony</td>
+                                        <td>14.03.2014</td>
+                                        <td>04.04.2014</td>
+                                </tr>
+                                <tr class="table-clickable-project">
+                                        <td>Gra komputerowa</td>
+                                        <td>131</td>
+                                        <td>gra</td>
+                                        <td>..</td>
+                                        <td>NIE</td>
+                                        <td>W trakcie realizacji</td>
+                                        <td>20.03.2014</td>
+                                        <td>21.03.2014</td>
+                                        
+                                </tr>
+                               
+                        </tbody>
+                </table>
+                </div>
+        </div>
+</div>
