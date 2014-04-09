6038a4c07cb5ef6345abc295a4f9f4c3f4c97a89
mnoworyta <m.noworyta@swiatgry.pl>
2014-04-09 23:36:59 +0200

    Update TypesOfIssues.php

--- pages/TypesOfIssues.php
+++ pages/TypesOfIssues.php
@@ -9,9 +9,10 @@
                 <table class="table table-hover table-striped">
                     <thead>
                         <tr>
-                            <th style="width: 60%">Typ zagadnienia</th>
-                            <th style="width: 20%">Sortuj</th>
-                            <th style="width: 20%">Akcja</th>
+                            <th style="width: 50%">Typ zagadnienia</th>
+                            <th style="width: 20%;">Zagadnienie widoczne na mapie:</th>
+                            <th style="width: 15%">Sortuj</th>
+                            <th style="width: 15%">Akcja</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -19,11 +20,8 @@
                             <td class="change">
                               <span data-toggle="modal" data-target="#myModal{{ issue.name || '' }}" editable-text="issue.name" e-name="name" e-form="rowform" onbeforesave="checkName($data, issue.id)" e-required e-placeholder="Nazwa zagadnienia" >
                               {{ issue.name || '' }}
-                              </span>
-                              <span editable-select="issue.map" e-name="map" e-form="rowform" e-ng-options="m.value as m.text for m in maps"></span>
+                              </span> 
                               <span editable-checklist="issue.field" e-name="field" e-form="rowform" e-ng-options="f.value as f.text for f in fields"></span>
-                              
-                              
                               <div class="modal fade" id="myModal{{ issue.name }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
                                 <div class="modal-dialog">
                                     <div class="modal-content">
@@ -36,26 +34,31 @@
                                             <p><strong>Zagadnienie pokazywane na mapie:</strong> {{ showMap(issue) }}</p>
                                             <p><strong>Standard fields:</strong> </p>
                                                 <ul>
-                                                    <li>{{ showField(issue) }}</li>
+                                                    <!--<li>{{ showField(issue) }}</li>-->
+                                                    <li>Wersja docelowa</li>
+                                                    <li>Data zamknięcia</li>
                                                 </ul>
                                         </div>
                                         <div class="modal-footer">
-                                            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                            <button type="button" class="btn btn-default" data-dismiss="modal">Zamknij</button>
                                         </div>
                                     </div>
                                 </div>
                               </div>                             
                             </td>
                             <td>
-                                <a href='#'><i class="fa fa-sort-amount-asc "></i></a>
+                                <span editable-checkbox="issue.map" e-name="map" e-form="rowform" onbeforesave="checkName($data, issue.id)" >
+                                {{ issue.map && "x" || "" }}
+                                </span>
+                            </td>
+                            <td>
                                 <a href='#'><i class="fa fa-sort-asc"></i></a>
                                 <a href='#'><i class="fa fa-sort-desc"></i></a>
-                                <a href='#'><i class="fa fa-sort-amount-desc"></i></a>
                             </td>
                             <td style="white-space: nowrap">
                                 <form editable-form name="rowform" onbeforesave="save($data, issue.id)" ng-show="rowform.$visible" class="form-buttons form-inline" shown="inserted == issue">
                                     <button type="submit" ng-disabled="rowform.$waiting" class="btn btn-primary"><i class="fa fa-floppy-o"></i></button>
-                                    <button type="button" ng-disabled="rowform.$waiting" ng-click="rowform.$cancel()" class="btn btn-default"><i class="fa fa-repeat"></i></button>
+                                    <button type="button" ng-disabled="rowform.$waiting" ng-click="rowform.$cancel()" class="btn btn-default"><i class="fa fa-ban"></i></button>
                                 </form>
                                 <div class="buttons" ng-show="!rowform.$visible">
                                     <button class="btn btn-primary" ng-click="rowform.$show()"><i class="fa fa-pencil-square-o"></i></button>
