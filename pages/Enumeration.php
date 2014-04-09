95da08a84503b593266807d26402fd1754b1daa5
mnoworyta <m.noworyta@swiatgry.pl>
2014-04-09 23:36:04 +0200

    Update Enumeration.php

--- pages/Enumeration.php
+++ pages/Enumeration.php
@@ -14,15 +14,44 @@
                 <div class="center-block">
                     <div ng-app="app" ng-controller="Ctrl">
                     <!-- Kategorie dokumentów -->
-                    <h3>Kategorie dokumentów</h3>
+                        <h3>Kategorie dokumentów</h3>
+                        <div class="add_enumeration">
+                            <button class="btn btn-primary" data-toggle="modal" data-target="#addEnumeration"><i class="fa fa-plus-square"></i> Dodaj nowy słownik</button>
+                        </div>
+                        
+                        <div class="modal fade" id="addEnumeration" tabindex="-1" role="dialog" aria-labelledby="addEnumerationLabel" aria-hidden="true">
+                            <div class="modal-dialog">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                        <h4 class="modal-title" id="addEnumerationLabel">Dodawanie nowego słownika</h4>
+                                    </div>
+                                    <div class="modal-body">
+                                     <label>Nazwa nowego słownika</label><br />
+                                     <input type="text" placeholder="Wpisz nazwę nowego słownika" class="form-control">
+                                     <br /><br /><br />
+                                     <label>Nazwa użytkownika</label><br />
+                                          <select class="form-control">
+                                            <option>Username #1</option>
+                                            <option>Username #2</option>
+                                            <option>Username #3</option>
+                                          </select>
+                                    </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-default" data-dismiss="modal">Zamknij</button>
+                                    <button type="button" class="btn btn-primary">Zapisz zmiany</button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 				    <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th>Nazwa</th>
-                                <th>Domyślny status</th>
-                                <th>Aktywne</th>
-                                <th>Sortuj</th>
-                                <th>Akcja</th>
+                                <th style="width: 40%;">Nazwa</th>
+                                <th style="width: 15%">Domyślny Status</th>
+                                <th style="width: 15%">Aktywne</th>
+                                <th style="width: 15%">Sortuj</th>
+                                <th style="width: 15%">Akcja</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -33,25 +62,21 @@
                                     </span></a>
                                 </td>
                                 <td>
-                                    <span editable-checkbox="document.default" e-name="default" e-form="rowform" onbeforesave="checkName($data, default.id)" >
-                                    {{ document.default && "x" || "" }}
+                                    <input type="radio" name="defaultstatus" />
+                                </td>
+                                <td>
+                                    <span editable-checkbox="document.map" e-name="map" e-form="rowform" onbeforesave="checkName($data, map.id)" >
+                                    {{ document.map && "x" || "" }}
                                     </span>
                                 </td>
                                 <td>
-                                    <span editable-checkbox="document.activity" e-name="default" e-form="rowform" onbeforesave="checkName($data, activity.id)" >
-                                    {{ document.activity && "x" || "" }}
-                                    </span>
-                                </td>
-                                <td>
-                                    <a href='#'><i class="fa fa-sort-amount-asc "></i></a>
                                     <a href='#'><i class="fa fa-sort-asc"></i></a>
                                     <a href='#'><i class="fa fa-sort-desc"></i></a>
-                                    <a href='#'><i class="fa fa-sort-amount-desc"></i></a>
                                 </td>
                                 <td style="white-space: nowrap">
                                     <form editable-form name="rowform" onbeforesave="save($data, document.id)" ng-show="rowform.$visible" class="form-buttons form-inline" shown="inserted == document">
                                         <button type="submit" ng-disabled="rowform.$waiting" class="btn btn-primary"><i class="fa fa-floppy-o"></i></button>
-                                        <button type="button" ng-disabled="rowform.$waiting" ng-click="rowform.$cancel()" class="btn btn-default"><i class="fa fa-repeat"></i></button>
+                                        <button type="button" ng-disabled="rowform.$waiting" ng-click="rowform.$cancel()" class="btn btn-default"><i class="fa fa-ban"></i></button>
                                     </form>
                                     <div class="buttons" ng-show="!rowform.$visible">
                                         <button class="btn btn-primary" ng-click="rowform.$show()"><i class="fa fa-pencil-square-o"></i></button>
@@ -68,11 +93,11 @@
 				    <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th>Nazwa</th>
-                                <th>Domyślny status</th>
-                                <th>Aktywne</th>
-                                <th>Sortuj</th>
-                                <th>Akcja</th>
+                                <th style="width: 40%;">Nazwa</th>
+                                <th style="width: 15%">Domyślny Status</th>
+                                <th style="width: 15%">Aktywne</th>
+                                <th style="width: 15%">Sortuj</th>
+                                <th style="width: 15%">Akcja</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -83,9 +108,7 @@
                                     </span></a>
                                 </td>
                                 <td>
-                                    <span editable-checkbox="priority.default" e-name="default" e-form="rowform" onbeforesave="checkName($data, default.id)" >
-                                    {{ priority.default && "x" || "" }}
-                                    </span>
+                                   <input type="radio" name="defaultstatus" />
                                 </td>
                                 <td>
                                     <span editable-checkbox="priority.activity" e-name="default" e-form="rowform" onbeforesave="checkName($data, activity.id)" >
@@ -93,15 +116,13 @@
                                     </span>
                                 </td>
                                 <td>
-                                    <a href='#'><i class="fa fa-sort-amount-asc "></i></a>
                                     <a href='#'><i class="fa fa-sort-asc"></i></a>
                                     <a href='#'><i class="fa fa-sort-desc"></i></a>
-                                    <a href='#'><i class="fa fa-sort-amount-desc"></i></a>
                                 </td>
                                 <td style="white-space: nowrap">
                                     <form editable-form name="rowform" onbeforesave="savepriority($data, priority.id)" ng-show="rowform.$visible" class="form-buttons form-inline" shown="inserted == priority">
                                         <button type="submit" ng-disabled="rowform.$waiting" class="btn btn-primary"><i class="fa fa-floppy-o"></i></button>
-                                        <button type="button" ng-disabled="rowform.$waiting" ng-click="rowform.$cancel()" class="btn btn-default"><i class="fa fa-repeat"></i></button>
+                                        <button type="button" ng-disabled="rowform.$waiting" ng-click="rowform.$cancel()" class="btn btn-default"><i class="fa fa-ban"></i></button>
                                     </form>
                                     <div class="buttons" ng-show="!rowform.$visible">
                                         <button class="btn btn-primary" ng-click="rowform.$show()"><i class="fa fa-pencil-square-o"></i></button>
@@ -119,11 +140,11 @@
 				    <table class="table table-striped">
                         <thead>
                             <tr>
-                                <th>Nazwa</th>
-                                <th>Domyślny status</th>
-                                <th>Aktywne</th>
-                                <th>Sortuj</th>
-                                <th>Akcja</th>
+                                <th style="width: 40%;">Nazwa</th>
+                                <th style="width: 15%">Domyślny Status</th>
+                                <th style="width: 15%">Aktywne</th>
+                                <th style="width: 15%">Sortuj</th>
+                                <th style="width: 15%">Akcja</th>
                             </tr>
                         </thead>
                         <tbody>
@@ -134,9 +155,7 @@
                                     </span></a>
                                 </td>
                                 <td>
-                                    <span editable-checkbox="time.default" e-name="default" e-form="rowform" onbeforesave="checkName($data, default.id)" >
-                                    {{ time.default && "x" || "" }}
-                                    </span>
+                                    <input type="radio" name="defaultstatus" />
                                 </td>
                                 <td>
                                     <span editable-checkbox="time.activity" e-name="default" e-form="rowform" onbeforesave="checkName($data, activity.id)" >
@@ -144,10 +163,8 @@
                                     </span>
                                 </td>
                                 <td>
-                                    <a href='#'><i class="fa fa-sort-amount-asc "></i></a>
                                     <a href='#'><i class="fa fa-sort-asc"></i></a>
                                     <a href='#'><i class="fa fa-sort-desc"></i></a>
-                                    <a href='#'><i class="fa fa-sort-amount-desc"></i></a>
                                 </td>
                                 <td style="white-space: nowrap">
                                     <form editable-form name="rowform" onbeforesave="savetime($data, time.id)" ng-show="rowform.$visible" class="form-buttons form-inline" shown="inserted == time">
@@ -164,6 +181,9 @@
                     </table>
                     <button class="btn btn-success" ng-click="addtime()"><i class="fa fa-plus-circle"></i> Nowa wartość</button>
                     <!-- Śledzenie czasu -->
+                    <div class="add_enumeration">
+                            <button class="btn btn-primary" data-toggle="modal" data-target="#addEnumeration"><i class="fa fa-plus-square"></i> Dodaj nowy słownik</button>
+                        </div>
                     </div>
 			     </div>
 			</div>
